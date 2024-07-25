496e5f0065dfab5e24d20203d624c9f119e10b29
merve.keles <merve.keles@orgino.com.tr>
2024-07-25 17:51:31 +0300

    exercise 10

--- assets/js/exercise/10.js
+++ assets/js/exercise/10.js
@@ -27,5 +27,59 @@ function classListOperations() {
       item.classList.remove('is-toggled');
     }
   })
+}
 
+/* 10g */
+function toggleOneElement(element) {
+  const toggleClassName = 'is-toggled';
+  const pointElements = document.querySelectorAll(".three-button");
+
+  pointElements.forEach((e) => {
+    if (e !== element && e.classList.contains(toggleClassName)) {
+      e.classList.remove(toggleClassName);
+    }
+  });
+  element.classList.add(toggleClassName);
+}
+
+/* 10h */
+function calculateTotal() {
+  const inputElement = document.querySelector('.cost-input');
+  const errorMessage = document.querySelector(".error-message");
+  const totalCost = document.querySelector('.total-cost')
+
+  let cost = Number(inputElement.value);
+
+  if (cost < 40) {
+    cost = cost + 10;
+  }
+
+  if (cost < 0) {
+    totalCost.style.display = "none";
+    errorMessage.style.display = "block";
+    errorMessage.innerHTML = "Error: cost can not be less than $0";
+  }
+  else {
+    errorMessage.style.display = "none";
+    totalCost.style.display = "block";
+    totalCost.innerHTML = `$${cost}`;
+  }
+}
+
+/* 10h */
+const getLocal = JSON.parse(localStorage.getItem('calculation'))
+console.log(getLocal);
+
+showCalculationInPage(getLocal)
+
+let calculation = "";
+function calculator(number) {
+  calculation += number;
+  console.log(calculation);
+  showCalculationInPage(calculation)
+}
+
+function showCalculationInPage(solution) {
+  const showCalculation = document.querySelector('.display-calculation');
+  showCalculation.innerHTML = solution;
 }
