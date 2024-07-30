49bec247d8c3123912b5a08e9dcf209049fa556a
merve.keles <merve.keles@orgino.com.tr>
2024-07-30 17:57:28 +0300

    lesson 11 part 2

--- assets/js/11-todo.js
+++ assets/js/11-todo.js
@@ -1,23 +1,39 @@
-const todoList = ['yemek ye', 'telefonunu şarj et', 'su iç'];
-let todoListHtml = '';
+const todoList = [
+  { name: 'yemek ye', dueDate: '2024-08-01' },
+  { name: 'telefonunu şarj et', dueDate: '2024-07-31' },
+  { name: 'su iç', dueDate: '2024-08-02' }
+];
+
+renderTodoList();
 
 function renderTodoList() {
+  let todoListHtml = '';
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`
+    const todoObj = todoList[i];
+    const { name, dueDate } = todoObj;
+    const html = `
+    <p>
+      ${name} ${dueDate}
+      <button onClick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+      ">Delete</button>
+    </p>
+    `
     todoListHtml += html;
   }
-
-  console.log(todoListHtml);
   document.querySelector('.todo-list').innerHTML = todoListHtml;
 }
 renderTodoList();
 
 function addTodo() {
   const todoNameInput = document.querySelector('.todo-name-input');
-  const todoNameValue = todoNameInput.value;
-  todoList.push(todoNameValue)
-  console.log(todoList);
+  const name = todoNameInput.value;
+
+  const todoDateInput = document.querySelector('.todo-date-input');
+  const dueDate = todoDateInput.value;
+
+  todoList.push({ name, dueDate });
 
   todoNameInput.value = '';
   renderTodoList();
